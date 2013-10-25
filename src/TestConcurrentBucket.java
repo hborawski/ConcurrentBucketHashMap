44872961316a5e62c4674c6c84904c3f1ae3a72d
Harris Borawski <harrisborawski@me.com>
2013-10-25 13:52:06 -0400

    changed from runnable to thread

--- src/TestConcurrentBucket.java
+++ src/TestConcurrentBucket.java
@@ -1,26 +1,28 @@
 
 public class TestConcurrentBucket {
 	public static void main(String[] args){
-		final ConcurrentBucketHashMap<Integer, Integer> map = new ConcurrentBucketHashMap<Integer, Integer>(3);
-		Runnable[] threads = new Runnable[5];
-		for(int i = 0; i < 5; i ++){
-			Runnable r = new Runnable(){
-	
-				@Override
-				public void run() {
-					// TODO Auto-generated method stub
-					for(int i=0; i < 10; i++){
-						map.put(i,i);
-					}
-					System.out.println(map.size());
-				}
-				
-			};
-			threads[i] = r;
+		final ConcurrentBucketHashMap<String, Integer> map = new ConcurrentBucketHashMap<String, Integer>(3);
+		Thread[] threads = new Thread[3];
+		for(int i = 0; i < 3; i ++){
+			SimpleThread t = new SimpleThread("Thread "+i, map);
+			threads[i]=t;
 		}
-		
-		for(Runnable r : threads){
-			r.run();
+		for(Thread run : threads){
+			run.start();
+		}
+	}
+	
+	private static class SimpleThread extends Thread{
+		private ConcurrentBucketHashMap<String, Integer> map;
+		public SimpleThread(String name, ConcurrentBucketHashMap<String, Integer> map){
+			super(name);
+			this.map = map;
+		}
+		public void run(){
+			for(int i = 0; i < 10; i ++){
+				map.put(this.getName()+i, i);
+			}
+			System.out.println(this.getName() + " " +map.size());
 		}
 	}
 }
